1fb4ce17e2ee57771bae07000db5d22443621642
liuxiang <liuxiang19890101@163.com>
2017-07-28 12:25:08 +0800

    updata

--- geekfac/src/main/java/com/geekid/geekfactest/MyApplication.java
+++ geekfac/src/main/java/com/geekid/geekfactest/MyApplication.java
@@ -1,17 +1,13 @@
 package com.geekid.geekfactest;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
-import android.graphics.Point;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.view.WindowManager;
 
 import com.geecare.blelibrary.BleTools;
 
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
 /**
@@ -45,43 +41,7 @@ public class MyApplication extends Application
         Log.d("lx","p:"+ dd);
 
     }
-
-    static double mInch;
-    public static double getScreenInch(Activity context) {
-        if (mInch != 0.0d) {
-            return mInch;
-        }
-
-        try {
-            int realWidth = 0, realHeight = 0;
-            Display display = context.getWindowManager().getDefaultDisplay();
-            DisplayMetrics metrics = new DisplayMetrics();
-            display.getMetrics(metrics);
-            if (android.os.Build.VERSION.SDK_INT >= 17) {
-                Point size = new Point();
-                display.getRealSize(size);
-                realWidth = size.x;
-                realHeight = size.y;
-            } else if (android.os.Build.VERSION.SDK_INT < 17
-                    && android.os.Build.VERSION.SDK_INT >= 14) {
-                Method mGetRawH = Display.class.getMethod("getRawHeight");
-                Method mGetRawW = Display.class.getMethod("getRawWidth");
-                realWidth = (Integer) mGetRawW.invoke(display);
-                realHeight = (Integer) mGetRawH.invoke(display);
-            } else {
-                realWidth = metrics.widthPixels;
-                realHeight = metrics.heightPixels;
-            }
-
-            mInch =formatDouble(Math.sqrt((realWidth/metrics.xdpi) * (realWidth /metrics.xdpi) + (realHeight/metrics.ydpi) * (realHeight / metrics.ydpi)),1);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return mInch;
-    }
+    
     /**
      * Double类型保留指定位数的小数，返回double类型（四舍五入）
      * newScale 为指定的位数
